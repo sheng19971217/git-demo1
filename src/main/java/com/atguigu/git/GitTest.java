5ffa5a2a36460c1790154c8423b45b4a5396d93f
yangsheng <122852540@qq.com>
2023-03-04 20:19:46 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;/*
+    @author yang
+    @create 2023-03-04 20:16
+*/
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+
+}
